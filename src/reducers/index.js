e2e12959284760c6fb6210ce528730f2e1074962
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-28 15:44:31 -0700

    Initialized reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer'
 
-export default combinReducers({
-  libraries: () => [];
+export default combineReducers({
+  libraries: LibraryReducer
 });
