600daedf87f51eada27f446d54722aeb805c10c4
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-28 17:38:52 -0700

    added ListItem component

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
+import ListItem from './ListItem';
+
 class LibraryList extends Component{
   componentWillMount() {
     const ds = new ListView.DataSource({ 
@@ -11,8 +13,8 @@ class LibraryList extends Component{
     this.dataSource = ds.cloneWithRows(this.props.libraries);
   }
 
-  renderRow() {
-
+  renderRow(library) {
+    return <ListItem library={library} />;
   }
 
   render() {
---
+++ src/components/ListItem.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class ListItem extends Component {
+  render() {
+
+  }
+}
+
+export default ListItem;
