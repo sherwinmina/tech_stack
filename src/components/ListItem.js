fa420355628f3fee4dbc8aefde944a9a68ffa186
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-28 23:12:06 -0700

    added layout animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, TouchableWithoutFeedback, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { library, expanded } = this.props;
     
     if (expanded) {
       return (
-        <Text>{library.description}</Text>
+        <CardSection>
+           <Text style={{ flex: 1 }}>
+              {library.description}
+            </Text>
+        </CardSection>
       );
     }
   }
